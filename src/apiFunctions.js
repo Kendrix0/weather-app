3f9f2fbcd97f46b72f9532bcb2ddf7e75daba75b
Kendrix0 <kevin.nguyen9917@gmail.com>
2021-12-13 12:43:16 -0600

    Basic form

--- src/apiFunctions.js
+++ src/apiFunctions.js
@@ -7,10 +7,14 @@ export let geocodeByName = async function(cityName, stateCode, countryCode, limi
     return geoData;
 };
 
-export let oneCallWeather = async function(cityName, stateCode='', countryCode='', limit=1) {
-    let coords = await geocodeByName(cityName, stateCode, countryCode, limit=1);
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coords[0].lat}&lon=${coords[0].lon}&exclude=minutely&appid=${apiKey}`);
-    let weatherData = await response.json();
-    console.log(weatherData);
-    return weatherData;
+export let oneCallWeather = async function(units, cityName, stateCode='', countryCode='', limit=1) {
+    try {
+        let coords = await geocodeByName(cityName, stateCode, countryCode, limit=1);
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coords[0].lat}&lon=${coords[0].lon}&units=${units}&exclude=minutely&appid=${apiKey}`);
+        let weatherData = await response.json();
+        console.log(weatherData);
+        return weatherData;
+    } catch(e) {
+        console.log('Error!', e);
+    }
 }
