7bdaf6f724e6a8afb8d7337793e9237deb017dfa
Kendrix0 <83741031+Kendrix0@users.noreply.github.com>
2025-01-15 20:25:52 -0600

    initialize configuration files

---
+++ webpack.prod.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new Dotenv()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+        ],
+    },
+};
