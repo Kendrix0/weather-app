f1c36a241923f3951a02b7ad0e744e104a8771ef
Kendrix0 <83741031+Kendrix0@users.noreply.github.com>
2025-01-19 14:25:22 -0600

    remove unneeded components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
+<script type="module">
+import { createApp, ref } from 'vue'
+</script>
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import WeatherAPI from './components/WeatherAPI.vue'
+import WeatherSummary from './components/WeatherSummary.vue'
+
+const location = ref('');
+const weatherData = ref('');
+const days = ref([]);
+const APIkey = ref('FMNUWVMF27AJNG8P9WAECF8RS');
+
+let startDate = new Date();
+let endDate = new Date();
+
+endDate = endDate.setDate(endDate.getDate() + 9);
+startDate = startDate.toISOString().substring(0, 10);
+endDate = new Date(endDate).toISOString().substring(0, 10);
+
+async function requestWeatherData() {
+  try {
+    let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location.value}/${startDate}/${endDate}?key=${APIkey.value}`);
+    weatherData.value = await response.json();
+    for (let day of weatherData.value.days) {
+      days.value.push(day);
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
 </script>
 
 <template>
@@ -14,7 +40,13 @@ import WeatherAPI from './components/WeatherAPI.vue'
   </header>
 
   <main>
-    <WeatherAPI />
+    <div>
+      <div>
+        <input type="text" placeholder="Location" v-model="location">
+        <button @click="requestWeatherData">Submit</button>
+      </div>
+    </div>
+    <WeatherSummary :days />
   </main>
 </template>
 
---
+++ src/components/WeatherSummary.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+    days: Array
+});
+
+</script>
+
+<template>
+    <ul>
+        <li v-for="day in days" :key="day.datetime">
+            {{ day.datetime }}
+        </li>
+    </ul>
+</template>
+
+<style></style>
