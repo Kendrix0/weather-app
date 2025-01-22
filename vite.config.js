ac5ae47f054c8a82e2845c65b6cd5f62ee8678af
Kendrix0 <83741031+Kendrix0@users.noreply.github.com>
2025-01-22 07:34:39 -0600

    update vite config for main branch use

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/weather-app/tree/weather-vue',
+  base: '/weather-app/',
   plugins: [
     VueRouter(),
     Vue({
