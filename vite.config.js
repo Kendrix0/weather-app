2cff0d129167c09ef7e3bc9c8b16612bbe1b8900
Kendrix0 <83741031+Kendrix0@users.noreply.github.com>
2025-01-22 07:23:34 -0600

    add base to vite config

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/weather-app/tree/weather-vue',
   plugins: [
     VueRouter(),
     Vue({
