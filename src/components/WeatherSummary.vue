8a6ee508d8bd240b7a2c40ef12bbde4d590823e5
Kendrix0 <83741031+Kendrix0@users.noreply.github.com>
2025-01-22 06:56:12 -0600

    import ref for dynamic assignment of selected day

--- src/components/WeatherSummary.vue
+++ src/components/WeatherSummary.vue
@@ -1,11 +1,18 @@
 <!-- eslint-disable vue/max-attributes-per-line -->
+<script type="module">
+import { ref } from 'vue'
+</script>
 <script setup>
 const props = defineProps({
     days: Array
 });
 
 const weekday = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]; 
-const weekdayFull = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+const weekdayFull = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const selectedDay = ref(props.days[0]);
+function setDay(num) {
+  selectedDay.value = props.days[num]
+}
 </script>
 
 <template>
@@ -15,31 +22,32 @@ const weekdayFull = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sa
     </div>
       <div class="v-row">
         <div class="v-col v-col-3">
-          <div class="v-sheet v-theme--light rounded-lg">
+          <div class="v-sheet rounded-lg">
             <div
-              class="v-list v-theme--light v-list--density-default v-list--one-line rounded-lg"
+              class="v-list v-list--density-default v-list--one-line rounded-lg"
               tabindex="0"
               role="listbox"
             >
               <div v-for="day in days" :key="day.datetime"
-                class="v-list-item v-list-item--link v-theme--light v-list-item--density-default v-list-item--one-line rounded-0 v-list-item--variant-text"
+                class="border-t border-b v-list-item v-list-item--link v-list-item--density-default v-list-item--one-line rounded-0 v-list-item--variant-text"
                 tabindex="-2"
+                @click="setDay(days.indexOf(day))"
               >
                 <span class="v-list-item__overlay"></span>
-                <span class="v-list-item__underlay"></span>
                 <div class="v-list-item__content" data-no-activator="">
-                  <div class="v-list-item-title">{{ day == days[0] ? "Today" : weekday[(new Date(day.datetime)).getDay()] }}: {{ day.tempmin }} - {{ day.tempmax }}</div>
+                  <div class="v-list-item-title">{{ day == days[0] ? "Today" : weekdayFull[(new Date(day.datetime)).getDay()] }}</div>
+                  <div class="v-list-item-subtitle">{{ day.tempmin }} - {{ day.tempmax }}</div>
                 </div>
               </div>
             </div>
           </div>
         </div>
           <div class="v-col">
-          <div class="v-sheet v-theme--light rounded-lg" style="min-height: 70vh">
-            <div>{{ weekdayFull[(new Date(days[0].datetime)).getDay()] }}</div>
-            <div>Currently: {{ days[0].temp }}</div>
-            <div>{{ days[0].conditions }}</div>
-            <div>High: {{ days[0].tempmax }} Low: {{ days[0].tempmin }}</div>
+          <div class="v-sheet rounded-lg" style="min-height: 70vh; padding: 3%;">
+            <h1>{{ weekdayFull[(new Date(selectedDay.datetime)).getDay()] }}</h1>
+            <div>Currently: {{ selectedDay.temp }}</div>
+            <div>{{ selectedDay.conditions }}</div>
+            <div>High: {{ selectedDay.tempmax }} Low: {{ selectedDay.tempmin }}</div>
             <div></div>
           </div>
         </div>
